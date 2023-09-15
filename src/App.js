73c074b6f61b84778a53c3c26b14d957303b6204
Jari-prg <hossainjarina@gmail.com>
2023-09-15 20:48:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <Sec/>
+      <Th/>
+      <Fo/>
+      <Fi/>
+    </div>
+  );
+}
+
+function First(){
+  return(
+    <div>
+      <h1>Hello Everyone</h1>
+    </div>
+  )
+}
+
+function Sec(){
+  return(
+    <div>
+    <h2>My name is Jarina Hossain</h2>
+    </div>
+  )
+}
+
+function Th(){
+  return(
+    <div>
+    <h2>This is my first ReactJs Program</h2>
+    </div>
+  )
+}
+
+function Fo(){
+  return(
+    <div>
+    <h2>I am from Future Institute of Engineering and management</h2>
+    </div>
+  )
+}
+
+function Fi(){
+  return(
+    <div>
+    <h2>I am currently in 3rd year persuing B.Tech in CSE</h2>
+    </div>
+  )
+}
+
+export default App;
